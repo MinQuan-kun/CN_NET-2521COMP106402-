57a725e48947ba95a0235ae1b68768a7b165d8bb
MinQuan-kun <182081176+MinQuan-kun@users.noreply.github.com>
2026-03-25 07:22:29 +0700

    Buoi06

---
+++ Buoi06/Buoi06/Data/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Buoi06.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        public DbSet<Loai> Loais { get; set; }
+        public DbSet<HangHoa> HangHoas { get; set; }
+    }
+}       
