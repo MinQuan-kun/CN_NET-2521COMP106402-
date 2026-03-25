41bc48e0dd776425ec04098a485daba232fd9936
MinQuan-kun <182081176+MinQuan-kun@users.noreply.github.com>
2026-03-25 07:21:48 +0700

    Buoi05

---
+++ Buoi05/Buoi05/ViewComponents/CategoryMenu.cs
@@ -0,0 +1,19 @@
+﻿using DemoLayout.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace Buoi05.ViewComponents
+{
+    public class CategoryMenu : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            //đọc database
+            var danhMuc = new List<Category>
+            {
+                new Category{Id=1, Name="Điện thoại"},
+                new Category{Id=2, Name="Máy tính bảng"},
+                new Category{Id=3, Name="Điều hòa"},
+            };
+            return View(danhMuc);
+        }
+    }
+}
